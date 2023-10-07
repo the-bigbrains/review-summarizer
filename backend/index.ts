cc5f462d5efc3eaf83da03952f272cff5d16e975
Sim Ho <simhozebs@gmail.com>
2023-10-07 11:53:29 -0400

    connect express with scraper

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import run from "./web";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  console.log("got request");
+app.get("/", async (req, res) => {
+  const { productUrl } = req.query;
 
-  res.send("Hello, world!");
+  if (!productUrl) {
+    res.send("empty productURL");
+    return;
+  }
+
+  const response = await run(productUrl as any);
+
+  console.log(response);
+
+  res.send(response);
 });
 
 app.listen(port, () => {
--- backend/web.ts
+++ backend/web.ts
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+
 async function run(url: string) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -13,12 +14,9 @@ async function run(url: string) {
       })
     )
   );
-  console.log(review);
-  console.log("hello");
 
   await browser.close(); // close browser
+  return review;
 }
 
-run(
-  "https://www.amazon.com/AmazonBasics-Pound-Neoprene-Dumbbells-Weights/dp/B01LR5S6HK/?_encoding=UTF8&pd_rd_w=VmDoS&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=QNMAB4MPATAA66QTZK7F&pd_rd_wg=1Ac1s&pd_rd_r=d22f6dfb-23d5-405e-acf3-c5dffa629d45&ref_=pd_gw_crs_zg_bs_3375251&th=1"
-);
+export default run;
