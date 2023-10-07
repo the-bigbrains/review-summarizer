77d4b3f3456cf03fe6645a3713c8e7fcf3f94b91
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-10-07 12:34:13 -0400

    negative review

--- extension/src/webscraping/negative.ts
+++ extension/src/webscraping/negative.ts
@@ -1,5 +1,24 @@
-async function negative(page: any) {
-  const link = 'a:contains("Critical reviews")';
-  await page.waitForSelector(link);
-  await page.click(link);
+export async function negative(page: any) {
+  const linkSelector = 'a[data-reftag="cm_cr_arp_d_viewpnt_rgt"]';
+  const link = await page.evaluate((selector: any) => {
+    const element = document.querySelector(selector);
+    return element ? element.href : null;
+  }, linkSelector);
+
+  // If the link is found, navigate to it
+  if (link) {
+    await page.goto(link);
+  } else {
+    console.log("Link not found on the page.");
+  }
+
+  const review = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('div[class="a-row a-spacing-small review-data"]'),
+      (element) => ({
+        text: element.textContent?.replace(/\n/g, "").trim() || "",
+      })
+    )
+  );
+  return review
 }
