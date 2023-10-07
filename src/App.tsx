ae2eee9ae5131213b81037efb147d5d174e85c55
dany <71732131+DanyPalma@users.noreply.github.com>
2023-10-07 00:56:34 -0400

    add description and edit app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="">
-      <div className="bg-slate-800">yo</div>
+      <div className="bg-slate-800 w-60 h-60 text-white">yo</div>
     </div>
   );
 }
