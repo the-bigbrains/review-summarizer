b4ff4cbd518df4611d41bd4b2e6807beff52f9de
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-10-07 08:40:32 -0400

    fix

--- extension/gpt.ts
+++ extension/gpt.ts
@@ -6,7 +6,16 @@ const openai = new OpenAI({
 
 export default async function gpt(attributes: string[]) {
   const completion = await openai.chat.completions.create({
-    messages: ["You are "],
+    messages: [
+      {
+        role: "user",
+        content: `You are an experienced online review judge tasked with summarizing product reviews. 
+        Provide concise and insightful summaries of user comments, highlighting both the pros and cons of the product. 
+        Keep your summaries short, clear, and accessible to users of all familiarity levels with the product. 
+        Inject a touch of humor and wit to maintain a light-hearted tone throughout:
+        ${attributes}`,
+      },
+    ],
     model: "gpt-3.5-turbo",
     temperature: 0.7,
   });
--- extension/src/web.ts
+++ extension/src/web.ts
@@ -1,10 +1,8 @@
 const puppeteer = require("puppeteer");
-async function run() {
+async function run(url: string) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://www.amazon.com/AmazonBasics-Pound-Neoprene-Dumbbells-Weights/dp/B01LR5S6HK/?_encoding=UTF8&pd_rd_w=VmDoS&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=QNMAB4MPATAA66QTZK7F&pd_rd_wg=1Ac1s&pd_rd_r=d22f6dfb-23d5-405e-acf3-c5dffa629d45&ref_=pd_gw_crs_zg_bs_3375251&th=1"
-  ); // go to url
+  await page.goto(url); // go to url
 
   const html = await page.content();
   const review = await page.evaluate(() =>
@@ -15,8 +13,9 @@ async function run() {
       })
     )
   );
+  console.log(review);
 
   await browser.close(); // close browser
 }
 
-export default run;
+run( "https://www.amazon.com/AmazonBasics-Pound-Neoprene-Dumbbells-Weights/dp/B01LR5S6HK/?_encoding=UTF8&pd_rd_w=VmDoS&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=QNMAB4MPATAA66QTZK7F&pd_rd_wg=1Ac1s&pd_rd_r=d22f6dfb-23d5-405e-acf3-c5dffa629d45&ref_=pd_gw_crs_zg_bs_3375251&th=1");
