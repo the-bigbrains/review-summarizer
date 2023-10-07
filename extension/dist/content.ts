bb637cf32f0335cc82543cee2bec5ba18606096d
dany <71732131+DanyPalma@users.noreply.github.com>
2023-10-07 02:09:55 -0400

    add dist

---
+++ extension/dist/content.ts
@@ -0,0 +1,7 @@
+const init = function () {
+  const elem = document.createElement("div");
+  elem.className = "text-red-500 bg-red-500 w-24 h-24";
+  elem.innerHTML = "this is a typescript file!!!!";
+  document.body.appendChild(elem);
+};
+init();
