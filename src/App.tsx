5fdb2f1f3db48d4a27892321a4a46ae9e7f81636
Sim Ho <simhozebs@gmail.com>
2023-10-07 00:50:02 -0400

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="">
+      <div className="bg-slate-800">yo</div>
     </div>
   );
 }
