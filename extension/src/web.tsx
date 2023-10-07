885c716729b7809ea0bc7d826bd4f68c185db695
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-10-07 02:46:02 -0400

    Add webscraping

---
+++ extension/src/web.tsx
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.amazon.com/AmazonBasics-Pound-Neoprene-Dumbbells-Weights/dp/B01LR5S6HK/?_encoding=UTF8&pd_rd_w=VmDoS&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=QNMAB4MPATAA66QTZK7F&pd_rd_wg=1Ac1s&pd_rd_r=d22f6dfb-23d5-405e-acf3-c5dffa629d45&ref_=pd_gw_crs_zg_bs_3375251&th=1"
+  );
+
+  const html = await page.content();
+  const review = await page.evaluate(() => Array.from(document.querySelectorAll('div[data-hook="review-collapsed"]'), element => element.textContent));
+  
+  console.log(review);
+
+
+
+  await browser.close(); // close browser
+}
+
+run();
