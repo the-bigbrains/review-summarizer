41334c6b4fd48eb2c26fbf2376468ca19fe50df9
dany <71732131+DanyPalma@users.noreply.github.com>
2023-10-07 11:51:39 -0400

    add fetchurl, add dev script, update package.json

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -6,10 +6,17 @@ function App() {
     const yeet = await fetch("http://localhost:3000");
   };
 
-  return (
-    <div className="">
-      <div className="bg-slate-800 w-60 h-60 text-white">yo yo</div>
-      <button onClick={() => test()}>Yeet</button>
+  return ( 
+    <div className="bg-slate-800 w-96 h-96 flex flex-col items-center justify-center text-white">
+      <div className="text-2xl">Review Rune</div>
+      <div>
+        <h1 className="text-xl">Please Enter your OpenAI API Key:</h1>
+        <div className="flex-row">
+          <input className="text-lg text-black text-center" type="text" placeholder="Enter API Key here..."></input>
+          <button className="bg-white text-black rounded-md px-1 mx-1 text-lg hover:bg-green-500 hover:text-white">Submit</button>
+        </div>
+        <button onClick={() => test()}>Yeet</button>
+      </div>
     </div>
   );
 }
