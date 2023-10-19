efe0f072fc4ac920f3e43ec9ae7ec730995a059f
Sim Ho <simhozebs@gmail.com>
2023-10-18 22:06:35 -0400

    DRY code & fix types

--- backend/webscraping/amazon/amazonScrape.ts
+++ backend/webscraping/amazon/amazonScrape.ts
@@ -1,5 +1,3 @@
-import { positive } from "./positiveReview";
-import { negative } from "./negativeReview";
 import puppeteer from "puppeteer";
 
 export default async function scrapeReviews(url: string) {
@@ -7,29 +5,61 @@ export default async function scrapeReviews(url: string) {
 
   const page = await browser.newPage();
   await page.goto(url); // go to url
-  const html = await page.content();
+  await page.content();
+
   // Use page.evaluate() to find the <a> tag with the specific data-hook attribute
   const linkSelector = 'a[data-hook="see-all-reviews-link-foot"]';
-  const link = await page.evaluate((selector: any) => {
-    const element = document.querySelector(selector);
+
+  const link = await page.evaluate((selector) => {
+    const element: HTMLAnchorElement | null = document.querySelector(selector);
     return element ? element.href : null;
   }, linkSelector);
 
-  // If the link is found, navigate to it
-  if (link) {
-    await page.goto(link);
-  } else {
-    console.log("Link not found on the page.");
+  if (!link) {
+    console.log("link not found on the page");
+    return;
   }
 
-  const review_positive: { text: string }[] = await positive(page); //Head over to top positive review page
-  const review_negative: { text: string }[] = await negative(page); //Head over to top negative review page
+  // If the link is found, navigate to it
+  await page.goto(link);
+
+  const reviewSelectorArray = ["lf", "rg"];
+  const reviewArrayResolved: string[][] = [];
+
+  for (const selector of reviewSelectorArray) {
+    const linkSelector = `a[data-reftag="cm_cr_arp_d_viewpnt_${selector}t"]`;
+
+    const link = await page.evaluate((selector) => {
+      const element: HTMLAnchorElement | null =
+        document.querySelector(selector);
+      return element ? element.href : null;
+    }, linkSelector);
+
+    // If the link is found, navigate to it
+    if (!link) {
+      console.log("Link not found on the page.");
+      return;
+    }
+
+    await page.goto(link);
+
+    const review = await page.evaluate(() =>
+      Array.from(
+        document.querySelectorAll(
+          'div[class="a-row a-spacing-small review-data"]'
+        ),
+        (element) => element.textContent?.replace(/\n/g, "").trim() || ""
+      )
+    );
+
+    reviewArrayResolved.push(review);
+  }
 
   await browser.close(); // close browser
   console.log("browser closed");
 
   return {
-    positive: review_positive,
-    negative: review_negative,
+    positive: reviewArrayResolved[0],
+    negative: reviewArrayResolved[1],
   };
 }
