b781bec943fcd5eb1f6db5c74842851da908cd37
Sim Ho <simhozebs@gmail.com>
2023-10-19 15:31:22 -0400

    remove any type

--- backend/webscraping/airbnb/airReview.ts
+++ backend/webscraping/airbnb/airReview.ts
@@ -1,10 +1,9 @@
-export async function airReview(page: any) {
+import { Page } from "puppeteer";
 
+export async function airReview(page: Page) {
   const review = await page.evaluate(() =>
     Array.from(
-      document.querySelectorAll(
-        'span[class="ll4r2nl dir dir-ltr"]'
-      ),
+      document.querySelectorAll('span[class="ll4r2nl dir dir-ltr"]'),
       (element) => ({
         text: element.textContent?.replace(/\n/g, "").trim() || "",
       })
