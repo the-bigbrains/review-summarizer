83c423084816ca8a484b74f254f53f680fb686d6
Sim Ho <simhozebs@gmail.com>
2023-11-17 12:49:58 -0500

    inject walmart to a more fitting location

--- extension/injection/index.tsx
+++ extension/injection/index.tsx
@@ -10,44 +10,61 @@ elem.className = "w-full px-5 flex justify-center items-center";
 
 console.log("site:", site);
 
-let section: Element | HTMLCollection | null = null;
-switch (site) {
-  case "amazon":
-    section = document.getElementById("ask-btf_feature_div");
-    break;
-  case "yelp":
-    section = document.getElementsByClassName("css-13merx8");
-    for (let i = 0; i < section.length; i++) {
-      if (section[i].textContent === "Recommended Reviews") {
-        section = section[i];
-        break;
-      }
-    }
-    break;
-  case "walmart":
-    section = document.getElementsByClassName("f2 dib lh-title ma0 dark-gray");
-    break;
-  case "target":
-    section = document.getElementsByClassName(
-      "styles__StyledHeading-sc-1xmf98v-0 jhKFiw h-text-center h-padding-a-default h-margin-b-none"
-    );
-    for (let i = 0; i < section.length; i++) {
-      if (section[i].textContent === "Guest Ratings & Reviews") {
-        section = section[i];
-        break;
-      }
-    }
-    break;
-}
+const onLoad = () => {
+  console.log("site loaded!");
 
-console.log("section:", section);
+  let section: Element | HTMLCollection | null = null;
+  switch (site) {
+    case "amazon":
+      section = document.getElementById("ask-btf_feature_div");
 
-if (section instanceof HTMLElement) {
-  section.appendChild(elem);
-} else if (section instanceof HTMLCollection) {
-  section[0].appendChild(elem);
-}
-console.log("after", section);
-//section?.appendChild(elem);
-const rootDiv = ReactDOM.createRoot(elem);
-rootDiv.render(<App />);
+      if (section instanceof HTMLElement) {
+        section.appendChild(elem);
+      } else if (section instanceof HTMLCollection) {
+        section[0].appendChild(elem);
+      }
+      break;
+    case "yelp":
+      section = document.getElementsByClassName("css-13merx8");
+      for (let i = 0; i < section.length; i++) {
+        if (section[i].textContent === "Recommended Reviews") {
+          section = section[i];
+          break;
+        }
+      }
+      break;
+    case "walmart":
+      section = document.getElementById("item-review-section");
+      if (!section) return;
+
+      const ratingsSection = section.childNodes[1];
+
+      section.insertBefore(elem, ratingsSection);
+
+      break;
+    case "target":
+      section = document.getElementsByClassName(
+        "styles__StyledHeading-sc-1xmf98v-0 jhKFiw h-text-center h-padding-a-default h-margin-b-none"
+      );
+      for (let i = 0; i < section.length; i++) {
+        if (section[i].textContent === "Guest Ratings & Reviews") {
+          section = section[i];
+          break;
+        }
+      }
+
+      if (section instanceof HTMLElement) {
+        section.appendChild(elem);
+      } else if (section instanceof HTMLCollection) {
+        section[0].appendChild(elem);
+      }
+      break;
+  }
+
+  //section?.appendChild(elem);
+  const rootDiv = ReactDOM.createRoot(elem);
+  rootDiv.render(<App />);
+  window.removeEventListener("load", onLoad);
+};
+
+window.addEventListener("load", onLoad);
