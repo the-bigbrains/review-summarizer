6af33d0f1ead86381ba6a86ab3de9b04d7227143
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-11-17 15:50:28 -0500

    Collect more reviews for target

--- backend/webscraping/target/targetReview.ts
+++ backend/webscraping/target/targetReview.ts
@@ -4,10 +4,28 @@ export default async function targetReview(url: string) {
   const { browser, page } = await usePuppeteer(url); // use puppeteer to open a browser and a page
 
   await page.evaluate(() => {
-    window.scrollBy(0, 4000); // Adjust the scroll distance as needed
+    window.scrollBy(0, 7000); // Adjust the scroll distance as needed
   });
 
-  await page.waitForSelector('div[data-test="review-card--text"]');
+  const buttonSelector =
+    "#pageBodyContainer > div > div:nth-child(3) > div.h-text-center.h-padding-h-default.h-margin-t-default.h-margin-b-wide > div.h-text-center.h-padding-v-tight > button";
+
+  try { 
+    const buttonElement = await page.waitForSelector(buttonSelector);
+    if (buttonElement) {
+      await Promise.all([
+        buttonElement.click(),
+        page.waitForSelector('div[data-test="review-card--text"]', {
+          timeout: 30000,
+        }), // Wait for a review element to appear after button click
+      ]);
+      console.log("Clicked the button and reviews loaded");
+    } else {
+      console.log("Button not found");
+    }
+  } catch (error) {
+    console.error("Error clicking the button or waiting for reviews:", error);
+  }
 
   const reviewText = await page.$$eval(
     'div[data-test="review-card--text"]',
@@ -19,6 +37,7 @@ export default async function targetReview(url: string) {
   );
 
   await browser.close(); // close browser
+  console.log(reviewText);
 
   return {
     positive: reviewText.map((text) => text.text),
