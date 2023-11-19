ae1826e1956be3fccbd443561e3b8f5d987107ef
Sim Ho <simhozebs@gmail.com>
2023-11-19 02:36:10 -0500

    refactor Target scrape and add char min

--- backend/webscraping/target/targetReview.ts
+++ backend/webscraping/target/targetReview.ts
@@ -1,4 +1,6 @@
+import { z } from "zod";
 import { usePuppeteer } from "../customHooks/usePuppeteer";
+import reviewCharMin from "../util";
 
 export default async function targetReview(url: string) {
   const { browser, page } = await usePuppeteer(url); // use puppeteer to open a browser and a page
@@ -10,37 +12,36 @@ export default async function targetReview(url: string) {
   const buttonSelector =
     "#pageBodyContainer > div > div:nth-child(3) > div.h-text-center.h-padding-h-default.h-margin-t-default.h-margin-b-wide > div.h-text-center.h-padding-v-tight > button";
 
-  try { 
+  try {
     const buttonElement = await page.waitForSelector(buttonSelector);
-    if (buttonElement) {
-      await Promise.all([
-        buttonElement.click(),
-        page.waitForSelector('div[data-test="review-card--text"]', {
-          timeout: 30000,
-        }), // Wait for a review element to appear after button click
-      ]);
+    if (!buttonElement) throw new Error("Button not found");
+
+    await buttonElement.click();
+
+    await page.waitForSelector('div[data-test="review-card--text"]'), // Wait for a review element to appear after button click
       console.log("Clicked the button and reviews loaded");
-    } else {
-      console.log("Button not found");
-    }
   } catch (error) {
     console.error("Error clicking the button or waiting for reviews:", error);
   }
 
   const reviewText = await page.$$eval(
     'div[data-test="review-card--text"]',
-    (elements) => {
-      return elements.map((element) => {
-        return { text: element.textContent || "" } as { text: string }; // Use type assertion to ensure non-null string
-      });
-    }
+    (elements) => elements.map((element) => element.textContent?.trim())
   );
 
   await browser.close(); // close browser
   console.log(reviewText);
 
-  return {
-    positive: reviewText.map((text) => text.text),
-    negative: reviewText.map((text) => text.text),
-  };
+  try {
+    let reviewArray = z.array(z.string()).parse(reviewText);
+
+    reviewArray = reviewArray.filter((review) => review.length > reviewCharMin);
+
+    return {
+      positive: reviewArray,
+      negative: reviewArray,
+    };
+  } catch (e) {
+    console.log(e);
+  }
 }
